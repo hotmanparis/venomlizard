35c585c9cb5637e5998afa94cfd49d2bfb7d7376
hotmanparis <58946567+hotmanparis@users.noreply.github.com>
2021-08-22 00:57:53 +0200

    Update COCOVG_pretrain_VLT5_overfit.sh

--- VL-T5/scripts/COCOVG_pretrain_VLT5_overfit.sh
+++ VL-T5/scripts/COCOVG_pretrain_VLT5_overfit.sh
@@ -10,7 +10,7 @@ python -m torch.distributed.launch \
         --distributed --multiGPU --fp16 \
         --train mscoco_resplit_train_overfit \
         --valid mscoco_resplit_train_overfit \
-        --batch_size 1 \
+        --batch_size 8 \
         --optim adamw \
         --warmup_ratio 0.05 \
         --lr 1e-4 \
