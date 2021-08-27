bbc9a84bfa7dbc9a6ba2bd3dc04ac1379cdef585
Stefan Lionar <splionar@gmail.com>
2021-08-28 01:36:56 +0200

    sh

--- VLT5/scripts/COCOVG_pretrain_VLT5_overfit.sh
+++ VLT5/scripts/COCOVG_pretrain_VLT5_overfit.sh
@@ -1,7 +1,7 @@
 # The name of experiment
 name=VLT5
 
-output=snap/pretrain_no_reset_untie_groupnorm/$name
+output=snap/pretrain_no_reset_tie_groupnormL/$name
 
 PYTHONPATH=$PYTHONPATH:./src \
 python -m torch.distributed.launch \
