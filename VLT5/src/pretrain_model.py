822e274a63f70e373457b7c074cd731508cddfa3
Stefan Lionar <splionar@gmail.com>
2021-08-27 23:50:58 +0200

    diff

--- VLT5/src/pretrain_model.py
+++ VLT5/src/pretrain_model.py
@@ -61,8 +61,11 @@ class VLT5Pretraining(VLT5):
                 results[f'{task}_loss'] = task_loss[task]
                 results[f'{task}_loss_count'] = task_counts[task]
 
-        diff = torch.sum(self.encoder.embed_tokens.weight - self.decoder.embed_tokens.weight)
-        print("Embedding diff: {}".format(diff))
+        diff1 = torch.sum(self.encoder.embed_tokens.weight - self.decoder.embed_tokens.weight)
+        diff2 = torch.sum(self.decoder.embed_tokens.weight - self.lm_head.weight)
+        print("Encoder decoder embedding diff: {}".format(diff1))
+        print("Decoder Out embedding diff: {}".format(diff2))
+
         return results
 
     @torch.no_grad()
