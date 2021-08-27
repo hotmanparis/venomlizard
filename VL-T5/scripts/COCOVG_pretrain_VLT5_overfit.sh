e03424964e72962e9d950bf591646826d78d5952
Stefan Lionar <splionar@gmail.com>
2021-08-27 17:54:17 +0200

    config ori overfit

--- VL-T5/scripts/COCOVG_pretrain_VLT5_overfit.sh
+++ VL-T5/scripts/COCOVG_pretrain_VLT5_overfit.sh
@@ -10,11 +10,11 @@ python -m torch.distributed.launch \
         --distributed --multiGPU --fp16 \
         --train mscoco_resplit_train_overfit \
         --valid mscoco_resplit_train_overfit \
-        --batch_size 8 \
+        --batch_size 80 \
         --optim adamw \
         --warmup_ratio 0.05 \
         --lr 1e-4 \
-        --num_workers 8 \
+        --num_workers 1 \
         --clip_grad_norm 1.0 \
         --losses 'lm,qa,ground_caption,refer,itm' \
         --backbone 't5-base' \
